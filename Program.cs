ff22d69b7ad5bb288243a2da7ab316c008d60937
bairog <44995181+bairog@users.noreply.github.com>
2025-09-11 08:51:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SQLite;
+
+namespace System.Data.SQLite2.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new SQLiteConnectionStringBuilder();
+            builder.DataSource = "test.db";
+            using (var conn = new SQLiteConnection(builder.ToString()))
+            using (var comand = conn.CreateCommand())
+            {
+                conn.Open();
+                comand.CommandText = "SELECT NameValue from Products";
+                using (var reader = comand.ExecuteReader())
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0));
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
